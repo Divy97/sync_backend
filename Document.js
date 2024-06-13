e8bef25bfc047db0a6763306b093ad08dd720fec
Divy <divyparekh1810@gmail.com>
2024-06-13 18:50:10 +0530

    bug fixed

--- Document.js
+++ Document.js
@@ -1,10 +1,13 @@
-    const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-    const DocumentSchema = new mongoose.Schema({
-    _id: String,
-    data: Object,
-    owner: String, 
-    isEditable: Boolean
-    });
+const DocumentSchema = new mongoose.Schema({
+  _id: String,
+  data: Object,
+  owner: String,
+  isEditable: {
+    type: Boolean,
+    default: false,
+  },
+});
 
-    module.exports = mongoose.model("Document", DocumentSchema);
+module.exports = mongoose.model("Document", DocumentSchema);
--- server.js
+++ server.js
@@ -6,25 +6,22 @@ const app = express();
 const { Server } = require("socket.io");
 const cors = require("cors");
 app.use(cors());
-// const createNonStreamingMultipartContent  = require("./utils/vertexAi");
-const Document = require("./Document");
 
+const Document = require("./Document");
 
 const server = require("http").createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-  },  
-  transports: ['websocket'], 
+  },
+  transports: ["websocket"],
 });
-
 
 app.use(bodyParser.json());
 
 mongoose.connect(
-  "mongodb+srv://divyparekh1810:divyparekh1810@cluster0.3hrodsw.mongodb.net/documents",
-
+  "mongodb+srv://divyparekh1810:divyparekh1810@cluster0.3hrodsw.mongodb.net/documents"
 );
 
 io.on("connection", (socket) => {
@@ -35,7 +32,7 @@ io.on("connection", (socket) => {
         _id: documentId,
         data: {},
         owner: username,
-        isEditable: isEditable,
+        isEditable: Boolean(isEditable), // Cast to Boolean
       });
     }
 
@@ -46,38 +43,19 @@ io.on("connection", (socket) => {
       isEditable: document.isEditable,
     });
 
-    socket.on("send-changes", async (delta, username) => {
+    socket.on("send-changes", async (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
     });
 
-    socket.on("save-document", async (data, username) => {
-      if (document.owner === username) {
-        await Document.findByIdAndUpdate(documentId, { data });
-      }
+    socket.on("save-document", async (data) => {
+      await Document.findByIdAndUpdate(documentId, { data });
     });
 
-    socket.on("send-generated-text", (generatedText, username) => {
-      socket.broadcast
-        .to(documentId)
-        .emit("receive-generated-text", generatedText);
+    socket.on("send-generated-text", (generatedText) => {
+      socket.broadcast.to(documentId).emit("receive-generated-text", generatedText);
     });
   });
 });
-
-// app.post("/generate-text", async (req, res) => {
-//   try {
-//     const { text } = req.body;
-//     if (!text) {
-//       throw new Error("Text is required");
-//     }
-//     // const response = await createNonStreamingMultipartContent(text);
-//     res.status(200).json({ status: "success", response });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ error: "Something went wrong" });
-//   }
-// });
-
 
 server.listen(3001, () => {
   console.log("Server running on port 3001");
